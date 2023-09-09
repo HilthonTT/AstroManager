b2be3400c8de342cf2c24a69fbd9f6e603c0f33f
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-09 13:45:59 +0200

    Implemented SqlDataAccess

---
+++ AstroManagerApi.Library/DataAccess/Interfaces/ISqlDataAccess.cs
@@ -0,0 +1,9 @@
+﻿using Dapper;
+
+namespace AstroManagerApi.Library.DataAccess.Interfaces;
+public interface ISqlDataAccess
+{
+    Task<List<T>> LoadDataAsync<T>(string storedProcedure, DynamicParameters parameters = null);
+    Task<T> LoadFirstOrDefaultDataAsync<T>(string storedProcedure, DynamicParameters parameters = null);
+    Task SaveDataAsync(string storedProcedure, DynamicParameters parameters = null);
+}
---
+++ AstroManagerApi.Library/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,52 @@
+﻿using AstroManagerApi.Library.DataAccess.Interfaces;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace AstroManagerApi.Library.DataAccess;
+public class SqlDataAccess : ISqlDataAccess
+{
+    private const string DatabaseName = "AstroManagerData";
+    private readonly string _connectionString;
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+        _connectionString = GetConnectionString();
+    }
+
+    private string GetConnectionString()
+    {
+        return _config.GetConnectionString(DatabaseName);
+    }
+
+    public async Task<List<T>> LoadDataAsync<T>(string storedProcedure, DynamicParameters parameters = default)
+    {
+        using var connection = new SqlConnection(_connectionString);
+
+        var rows = await connection.QueryAsync<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+
+        return rows.ToList();
+    }
+
+    public async Task<T> LoadFirstOrDefaultDataAsync<T>(string storedProcedure, DynamicParameters parameters = default)
+    {
+        using var connection = new SqlConnection(_connectionString);
+
+        var element = await connection.QueryFirstOrDefaultAsync<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+
+        return element;
+    }
+
+    public async Task SaveDataAsync(string storedProcedure, DynamicParameters parameters = default)
+    {
+        using var connection = new SqlConnection(_connectionString);
+
+        await connection.ExecuteAsync(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+    }
+}
