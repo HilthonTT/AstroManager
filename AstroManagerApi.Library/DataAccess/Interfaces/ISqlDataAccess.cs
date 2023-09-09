cfc6a66d34d1f8f9713c5bbb7d97582b8fcf6c09
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-09 14:53:04 +0200

    Implemented Transactions for SqlDataAccess

--- AstroManagerApi.Library/DataAccess/Interfaces/ISqlDataAccess.cs
+++ AstroManagerApi.Library/DataAccess/Interfaces/ISqlDataAccess.cs
@@ -3,7 +3,13 @@
 namespace AstroManagerApi.Library.DataAccess.Interfaces;
 public interface ISqlDataAccess
 {
+    void CommitTransaction();
     Task<List<T>> LoadDataAsync<T>(string storedProcedure, DynamicParameters parameters = null);
+    Task<List<T>> LoadDataInTransactionAsync<T>(string storedProcedure, DynamicParameters parameters = null);
     Task<T> LoadFirstOrDefaultDataAsync<T>(string storedProcedure, DynamicParameters parameters = null);
+    Task<T> LoadFirstOrDefaultInTransactionAsync<T>(string storedProcedure, DynamicParameters parameters = null);
+    void RollbackTransaction();
     Task SaveDataAsync(string storedProcedure, DynamicParameters parameters = null);
+    Task SaveDataInTransactionAsync(string storedProcedure, DynamicParameters parameters = null);
+    void StartTransaction();
 }
--- AstroManagerApi.Library/DataAccess/SqlDataAccess.cs
+++ AstroManagerApi.Library/DataAccess/SqlDataAccess.cs
@@ -10,6 +10,8 @@ public class SqlDataAccess : ISqlDataAccess
     private const string DatabaseName = "AstroManagerData";
     private readonly string _connectionString;
     private readonly IConfiguration _config;
+    private static IDbConnection _connection;
+    private static IDbTransaction _transaction;
 
     public SqlDataAccess(IConfiguration config)
     {
@@ -20,6 +22,15 @@ public class SqlDataAccess : ISqlDataAccess
     private string GetConnectionString()
     {
         return _config.GetConnectionString(DatabaseName);
+    }
+
+    private static void Dispose()
+    {
+        _transaction?.Dispose();
+        _connection?.Dispose();
+
+        _transaction = null;
+        _connection = null;
     }
 
     public async Task<List<T>> LoadDataAsync<T>(string storedProcedure, DynamicParameters parameters = default)
@@ -48,5 +59,51 @@ public class SqlDataAccess : ISqlDataAccess
 
         await connection.ExecuteAsync(storedProcedure, parameters,
             commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveDataInTransactionAsync(string storedProcedure, DynamicParameters parameters = default)
+    {
+        await _connection?.ExecuteAsync(storedProcedure, parameters, 
+            commandType: CommandType.StoredProcedure, transaction: _transaction);
+    }
+
+    public async Task<List<T>> LoadDataInTransactionAsync<T>(string storedProcedure, DynamicParameters parameters = default)
+    {
+        var rows = await _connection?.QueryAsync<T>(storedProcedure, parameters, 
+            commandType: CommandType.StoredProcedure, transaction: _transaction);
+
+        return rows.ToList();
+    }
+
+    public async Task<T> LoadFirstOrDefaultInTransactionAsync<T>(string storedProcedure, DynamicParameters parameters = null)
+    {
+        var element = await _connection?.QueryFirstOrDefaultAsync<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure, transaction: _transaction);
+
+        return element;
+    }
+
+    public void StartTransaction()
+    {
+        _connection = new SqlConnection(_connectionString);
+        _connection.Open();
+
+        _transaction = _connection.BeginTransaction();
+    }
+
+    public void CommitTransaction()
+    {
+        _transaction?.Commit();
+        _connection?.Close();
+
+        Dispose();
+    }
+
+    public void RollbackTransaction()
+    {
+        _transaction?.Rollback();
+        _connection?.Close();
+
+        Dispose();
     }
 }
