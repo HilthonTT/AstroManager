63479394c4003740afc2e710e5a5e47f3d961124
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-10 14:11:39 +0200

    Implemented API Controllers Part 1

---
+++ AstroManagerApi/Controllers/EntityController.cs
@@ -0,0 +1,66 @@
+﻿using AstroManagerApi.Library.DataAccess.Interfaces;
+using AstroManagerApi.Library.Enums;
+using AstroManagerApi.Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AstroManagerApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EntityController : ControllerBase
+{
+    private readonly ILogger<EntityController> _logger;
+    private readonly IEntityData _entityData;
+
+    public EntityController(ILogger<EntityController> logger, IEntityData entityData)
+    {
+        _logger = logger;
+        _entityData = entityData;
+    }
+
+    private ObjectResult LogError(string exceptionMessage, Operation operation)
+    {
+        string errorMessage = operation switch
+        {
+            Operation.GetAll => "There has been an issue while fetching the entities",
+            Operation.Create => "There has been an issue while creating the entities",
+            _ => "There has been an issue",
+        };
+
+        _logger.LogError("{message}: {e}", errorMessage, exceptionMessage);
+        return StatusCode(500, errorMessage);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllEntities()
+    {
+        try
+        {
+            var entities = await _entityData.GetAllEntitiesAsync();
+            return Ok(entities);
+        }
+        catch (Exception ex)
+        {
+            return LogError(ex.Message, Operation.GetAll);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateEntity([FromBody] EntityModel entity)
+    {
+        try
+        {
+            if (ModelState.IsValid is false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _entityData.CreateEntityAsync(entity);
+
+            return Ok("The entity has been created");
+        }
+        catch (Exception ex)
+        {
+            return LogError(ex.Message, Operation.Create);
+        }
+    }
+}
---
+++ AstroManagerApi/Controllers/UserController.cs
@@ -0,0 +1,94 @@
+﻿using AstroManagerApi.Library.DataAccess.Interfaces;
+using AstroManagerApi.Library.Enums;
+using AstroManagerApi.Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AstroManagerApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+    private readonly IUserData _userData;
+
+    public UserController(ILogger<UserController> logger, IUserData userData)
+    {
+        _logger = logger;
+        _userData = userData;
+    }
+
+    private ObjectResult LogError(string exceptionMessage, Operation operation)
+    {
+        string errorMessage = operation switch
+        {
+            Operation.GetByOid => "There has been an issue while fetching the user using the OID",
+            Operation.Create => "There has been an issue creating the user",
+            Operation.Update => "There has been an issue updating the user",
+            _ => "There has been an issue",
+        };
+
+        _logger.LogError("{message}: {e}", errorMessage, exceptionMessage);
+        return StatusCode(500, errorMessage);
+    }
+
+    [HttpGet("{oid}")]
+    public async Task<IActionResult> GetUserByOid(string oid)
+    {
+        try
+        {
+            var user = await _userData.GetUserByOidAsync(oid);
+            if (user is null)
+            {
+                return NotFound("No user has been found.");
+            }
+
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return LogError(ex.Message, Operation.GetByOid);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] UserModel user)
+    {
+        try
+        {
+            if (ModelState.IsValid is false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _userData.CreateUserAsync(user);
+
+            return Ok("The user has been created.");
+        }
+        catch (Exception ex)
+        {
+            return LogError(ex.Message, Operation.Create);
+        }
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateUser([FromBody] UserModel user)
+    {
+        try
+        {
+            if (ModelState.IsValid is false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _userData.UpdateUserAsync(user);
+
+            return Ok("The user has been updated.");
+        }
+        catch (Exception ex)
+        {
+            return LogError(ex.Message, Operation.Update);
+        }
+    }
+}
