be18003d6ad019c2a5cf67b2abf5a7a2867adcc9
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-13 16:10:31 +0200

    [Implementation] Dependency Injection .NET Maui

--- AstroManagerClient/MauiProgram.cs
+++ AstroManagerClient/MauiProgram.cs
@@ -1,22 +1,11 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace AstroManagerClient;
+﻿namespace AstroManagerClient;
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
+        builder.ConfigureServices();
 
         return builder.Build();
     }
---
+++ AstroManagerClient/RegisterServices.cs
@@ -0,0 +1,35 @@
+﻿using AstroManagerClient.Library.Api;
+using AstroManagerClient.Library.Api.Interfaces;
+using AstroManagerClient.Library.Models;
+using AstroManagerClient.Library.Models.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace AstroManagerClient;
+public static class RegisterServices
+{
+    public static void ConfigureServices(this MauiAppBuilder builder)
+    {
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        builder.Services.AddHttpClient();
+
+        builder.Services.AddSingleton<ILoggedInUser, UserModel>();
+        builder.Services.AddSingleton<IApiHelper, ApiHelper>();
+
+        builder.Services.AddTransient<ICredentialEndpoint, CredentialEndpoint>();
+        builder.Services.AddTransient<ICredentialTemplateEndpoint, CredentialTemplateEndpoint>();
+        builder.Services.AddTransient<IMasterPasswordEndpoint, MasterPasswordEndpoint>();
+        builder.Services.AddTransient<IRecoveryKeyEndpoint, RecoveryKeyEndpoint>();
+        builder.Services.AddTransient<ITypeEndpoint, TypeEndpoint>();
+        builder.Services.AddTransient<IUserEndpoint, UserEndpoint>();
+    }
+}
