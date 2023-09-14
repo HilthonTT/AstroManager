2b9a9d28bc74db8356436a6cc05fb3c651702c4a
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-14 19:37:03 +0200

    [Implementation] Added Services
    Added ISecureStorageWrapper
    Added Pages
    Added ViewModels
    Added Routings

--- AstroManagerClient/RegisterServices.cs
+++ AstroManagerClient/RegisterServices.cs
@@ -2,7 +2,11 @@
 using AstroManagerClient.Library.Api.Interfaces;
 using AstroManagerClient.Library.Models;
 using AstroManagerClient.Library.Models.Interfaces;
+using AstroManagerClient.Library.Storage;
+using AstroManagerClient.Library.Storage.Interfaces;
 using AstroManagerClient.ViewModels;
+using AstroManagerClient.Views;
+using CommunityToolkit.Maui;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -22,6 +26,7 @@ public static class RegisterServices
     {
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -37,6 +42,7 @@ public static class RegisterServices
         builder.Services.AddSingleton<ILoggedInUser, UserModel>();
         builder.Services.AddSingleton<IApiHelper, ApiHelper>();
 
+        builder.Services.AddTransient<ISecureStorageWrapper, SecureStorageWrapper>();
         builder.Services.AddTransient<ICredentialEndpoint, CredentialEndpoint>();
         builder.Services.AddTransient<ICredentialTemplateEndpoint, CredentialTemplateEndpoint>();
         builder.Services.AddTransient<IMasterPasswordEndpoint, MasterPasswordEndpoint>();
@@ -44,6 +50,12 @@ public static class RegisterServices
         builder.Services.AddTransient<ITypeEndpoint, TypeEndpoint>();
         builder.Services.AddTransient<IUserEndpoint, UserEndpoint>();
 
+        builder.Services.AddTransient<MainPage>();
+        builder.Services.AddTransient<LoginPage>();
+
         builder.Services.AddTransient<LoginViewModel>();
+
+        Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
     }
 }
