b5898382cc5e280b0528f4e520cf6029d7590d96
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-16 11:48:44 +0200

    Implemented Custom HorizontalWrapLayout

---
+++ AstroManagerClient/Layouts/HorizontalWrapLayout.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Maui.Layouts;
+
+namespace AstroManagerClient.Layouts;
+public class HorizontalWrapLayout : StackLayout
+{
+    protected override ILayoutManager CreateLayoutManager()
+    {
+        return new HorizontalWrapLayoutManager(this);
+    }
+}
