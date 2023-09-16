2a92bdc9e047eda5c54f1c1b04b455fa6d037d9d
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-16 13:40:14 +0200

    [Bug] Fixed AesEncryptor not working properly

--- AstroManagerApi.Library/Encryption/AesEncryptor.cs
+++ AstroManagerApi.Library/Encryption/AesEncryptor.cs
@@ -6,51 +6,61 @@ using System.Text;
 namespace AstroManagerApi.Library.Encryption;
 public class AesEncryptor : IAesEncryptor
 {
-    private readonly string _key;
-    private readonly string _iv;
+    private readonly Aes _aes;
     private readonly IConfiguration _config;
 
     public AesEncryptor(IConfiguration config)
     {
         _config = config;
 
-        _key = _config["Aes:Key"];
-        _iv = _config["Aes:IV"];
+        _aes = Aes.Create();
+        _aes.Key = StringToBytes(_config["Aes:KEY"]);
+        _aes.IV = StringToBytes(_config["Aes:IV"]);
+    }
+
+    private static byte[] StringToBytes(string text)
+    {
+        return Convert.FromBase64String(text);
     }
 
     public async Task<string> EncryptAsync(string plainText)
     {
-        using var aesAlg = Aes.Create();
+        byte[] encrypted;
 
-        aesAlg.Key = Encoding.UTF8.GetBytes(_key);
-        aesAlg.IV = Encoding.UTF8.GetBytes(_iv);
-
-        var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-        using var msEncrypt = new MemoryStream();
-        using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
-
-        using (var swEncrypt = new StreamWriter(csEncrypt))
+        using (ICryptoTransform encryptor = _aes.CreateEncryptor())
         {
-            await swEncrypt.WriteAsync(plainText);
+            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+
+            using var msEncrypt = new MemoryStream();
+            using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+            {
+                await csEncrypt.WriteAsync(plainBytes, 0, plainBytes.Length);
+                await csEncrypt.FlushFinalBlockAsync();
+            }
+
+            encrypted = msEncrypt.ToArray();
         }
 
-        return Convert.ToBase64String(msEncrypt.ToArray());
+        return Convert.ToBase64String(encrypted);
     }
 
     public async Task<string> DecryptAsync(string cipherText)
     {
-        using var aesAlg = Aes.Create();
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
 
-        aesAlg.Key = Encoding.UTF8.GetBytes(_key);
-        aesAlg.IV = Encoding.UTF8.GetBytes(_iv);
+        using ICryptoTransform decryptor = _aes.CreateDecryptor();
+        byte[] decryptedBytes;
 
-        var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+        using (var msDecrypt = new MemoryStream(cipherBytes))
+        {
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var ms = new MemoryStream();
+            await csDecrypt.CopyToAsync(ms);
+            decryptedBytes = ms.ToArray();
+        }
 
-        using var msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText));
-        using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+        string decryptedText = Encoding.UTF8.GetString(decryptedBytes).TrimEnd('\0');
 
-        using var srDecrypt = new StreamReader(csDecrypt);
-        return await srDecrypt.ReadToEndAsync();
+        return decryptedText;
     }
 }
