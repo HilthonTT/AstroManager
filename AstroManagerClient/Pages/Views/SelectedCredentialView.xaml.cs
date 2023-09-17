eafd8db7d7f1150ad602268216f3257be494e7d8
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-17 14:10:02 +0200

    [Minor Refactoring] SelectedCredentialView
    Moved code to SelectedCredentialViewModel

--- AstroManagerClient/Pages/Views/SelectedCredentialView.xaml.cs
+++ AstroManagerClient/Pages/Views/SelectedCredentialView.xaml.cs
@@ -1,6 +1,4 @@
-using AstroManagerClient.Models;
-using CommunityToolkit.Mvvm.Messaging;
-using System.ComponentModel;
+using AstroManagerClient.ViewModels;
 
 namespace AstroManagerClient.Pages.Views;
 
@@ -9,24 +7,6 @@ public partial class SelectedCredentialView : ContentView
     public SelectedCredentialView()
 	{
 		InitializeComponent();
-		BindingContext = this;
-
-		WeakReferenceMessenger.Default.Register<CredentialDisplayModel>(this, (r, m) =>
-		{
-			Credential = m;
-		});
-	}
-
-    public static readonly BindableProperty CredentialProperty = BindableProperty.Create(
-    nameof(CredentialProperty), typeof(CredentialDisplayModel), typeof(ContentView), new CredentialDisplayModel());
-
-    public CredentialDisplayModel Credential
-	{ 
-		get => (CredentialDisplayModel)GetValue(CredentialProperty);
-		set
-		{
-            SetValue(CredentialProperty, value);
-			OnPropertyChanged(nameof(Credential));
-        }
+		BindingContext = new SelectedCredentialViewModel();
 	}
 }
--- AstroManagerClient/ViewModels/SelectedCredentialViewModel.cs
+++ AstroManagerClient/ViewModels/SelectedCredentialViewModel.cs
@@ -1,18 +1,29 @@
 ﻿using AstroManagerClient.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 
 namespace AstroManagerClient.ViewModels;
 public partial class SelectedCredentialViewModel : BaseViewModel
 {
-    [ObservableProperty]
-    private CredentialDisplayModel _credential;
-
     public SelectedCredentialViewModel()
     {
         WeakReferenceMessenger.Default.Register<CredentialDisplayModel>(this, (r, m) =>
         {
             Credential = m;
         });
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsCredential))]
+    private CredentialDisplayModel _credential;
+
+    public bool IsCredential => Credential is not null;
+
+
+    [RelayCommand]
+    private void CloseCredential()
+    {
+        Credential = null;
     }
 }
