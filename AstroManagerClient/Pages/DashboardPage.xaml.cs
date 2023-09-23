d7eb821e293d2bd9a6ca2e23679cf4924054d2e0
Timothy Tandt <Timothy.tandt@outlook.be>
2023-09-23 12:44:01 +0200

    [Feature] Using actual credential data for Chart.

--- AstroManagerClient/Pages/DashboardPage.xaml.cs
+++ AstroManagerClient/Pages/DashboardPage.xaml.cs
@@ -1,3 +1,5 @@
+using AstroManagerClient.Library.Api.Interfaces;
+using AstroManagerClient.Library.Models.Interfaces;
 using AstroManagerClient.ViewModels;
 using Microcharts;
 using SkiaSharp.Views.Maui;
@@ -6,20 +8,43 @@ namespace AstroManagerClient.Pages;
 
 public partial class DashboardPage : ContentPage
 {
-	public DashboardPage(DashboardViewModel viewModel)
+    private readonly ICredentialEndpoint _credentialEndpoint;
+    private readonly ILoggedInUser _loggedInUser;
+
+    public DashboardPage(
+        DashboardViewModel viewModel,
+        ICredentialEndpoint credentialEndpoint,
+        ILoggedInUser loggedInUser)
 	{
 		InitializeComponent();
+
 		BindingContext = viewModel;
+        _credentialEndpoint = credentialEndpoint;
+        _loggedInUser = loggedInUser;
     }
 
-    protected override void OnAppearing()
+    protected override async void OnAppearing()
     {
-        base.OnAppearing();
-        DrawChart();
+        await DrawChartAsync();
     }
 
-    private void DrawChart()
+    private static float CalculateCount(float dataCount, int totalCount)
     {
+        return dataCount * 100 / totalCount;
+    }
+
+    private async Task DrawChartAsync()
+    {
+        var allCredentials = await _credentialEndpoint.GetUsersCredentialsAsync(_loggedInUser.Id);
+
+        float loginsCount = allCredentials.Where(x => x.Type.Name.Contains("Logins")).Count();
+        float passwordsCount = allCredentials.Where(x => x.Type.Name.Contains("Password")).Count();
+        float secureNoteCount = allCredentials.Where(x => x.Type.Name.Contains("Secure Note")).Count();
+        float creditCardCount = allCredentials.Where(x => x.Type.Name.Contains("Credit Card")).Count();
+        float identityCount = allCredentials.Where(x => x.Type.Name.Contains("Identity")).Count();
+
+        int totalCount = allCredentials.Count;
+
         TypesChart.Chart = new RadialGaugeChart
         {
             LabelTextSize = 24,
@@ -27,20 +52,25 @@ public partial class DashboardPage : ContentPage
             IsAnimated = true,
             Entries = new List<ChartEntry>
             {
-                new ChartEntry(10)
+                new ChartEntry(CalculateCount(loginsCount, totalCount))
                 {
-                    Color = Color.FromArgb("#EA7C69").ToSKColor()
+                    Color = Color.FromArgb("#EA7C69").ToSKColor(),
                 },
-                new ChartEntry(51)
+                new ChartEntry(CalculateCount(passwordsCount, totalCount))
                 {
                     Color = Color.FromArgb("#50D1AA").ToSKColor()
                 },
-                new ChartEntry(14)
+                new ChartEntry(CalculateCount(secureNoteCount, totalCount))
                 {
                     Color = Color.FromArgb("#FF7CA3").ToSKColor()
-                },new ChartEntry(2)
+                },
+                new ChartEntry(CalculateCount(creditCardCount, totalCount))
                 {
                     Color = Colors.WhiteSmoke.ToSKColor()
+                },
+                new ChartEntry(CalculateCount(identityCount, totalCount))
+                {
+                    Color = Colors.PaleVioletRed.ToSKColor()
                 }
             },
         };
